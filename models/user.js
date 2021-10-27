d0599a2020a4f10fa413f1869dff09624f7d11d2
hetthummar <hetthummar1234@gmail.com>
2021-10-27 16:28:48 +0530

    token_id name change to firebase_token_id

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
         immutable:true,
         minlength:10,
     },
-    token_id:{
+    firebase_token_id:{
         type:String,
         required:true,
     },
